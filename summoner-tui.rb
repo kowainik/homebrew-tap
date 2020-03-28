168b66731fbe98bd65fd13a360b0ae8bac3b6cfa
Veronika Romashkina <vrom911@gmail.com>
2020-03-28 16:32:16 +0000

    Fix summoner audit warnings

--- summoner-tui.rb
+++ summoner-tui.rb
@@ -1,17 +1,16 @@
 class SummonerTui < Formula
-  desc "TUI Tool for scaffolding fully configured batteries-included production-level Haskell projects"
+  desc "TUI Tool for scaffolding batteries-included Haskell projects"
   homepage "https://kowainik.github.io/projects/summoner"
   url "https://github.com/kowainik/summoner/releases/download/v2.0.0.0/summon-tui-osx"
   sha256 "0a264c29e142fc1bbc8b9be37aac10f896bb254558b42dc4c87c4535a4013146"
 
   bottle :unneeded
-  conflicts_with "summoner-cli", :because => "because both install 'summon' binaries"
   depends_on "hub"
+  conflicts_with "summoner-cli", :because => "because both install 'summon' binaries"
 
   def install
     bin.install "summon-tui-osx"
     mv "#{bin}/summon-tui-osx", "#{bin}/summon"
-
   end
 
   test do
