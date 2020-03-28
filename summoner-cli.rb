fe408dd5c6e32ff28535621b76ccb0ba45a24397
Veronika Romashkina <vrom911@gmail.com>
2020-03-28 16:23:16 +0000

    Add Summoner CLI and TUI formulaes (#4)

---
+++ summoner-cli.rb
@@ -0,0 +1,20 @@
+class SummonerCli < Formula
+  desc "CLI Tool for scaffolding fully configured batteries-included production-level Haskell projects"
+  homepage "https://kowainik.github.io/projects/summoner"
+  url "https://github.com/kowainik/summoner/releases/download/v2.0.0.0/summon-cli-osx"
+  sha256 "8f9b5e51e4365c2d0de05a51ec6e3a03351e9daff5b5e39dd0634b7ba646c04d"
+
+  bottle :unneeded
+  conflicts_with "summoner-tui", :because => "because both install 'summon' binaries"
+  depends_on "hub"
+
+  def install
+    bin.install "summon-cli-osx"
+    mv "#{bin}/summon-cli-osx", "#{bin}/summon"
+
+  end
+
+  test do
+    system "#{bin}/summon", "--version"
+  end
+end
