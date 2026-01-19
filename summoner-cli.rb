0668a6250e4612a1931802b511b128e737ecbaf0
Veronika Romashkina <vrom911@gmail.com>
2026-01-19 10:54:44 +0000

    Add macos-x86-64 binaries for summoner

--- summoner-cli.rb
+++ summoner-cli.rb
@@ -1,15 +1,25 @@
 class SummonerCli < Formula
   desc "CLI Tool for scaffolding batteries-included Haskell projects"
   homepage "https://kowainik.github.io/projects/summoner"
-  url "https://github.com/kowainik/summoner/releases/download/v2.2.0.0/summon-cli-macos-arm64"
-  sha256 "ac687412d4a51d403df514ed581d72982c7bc924710e73db6a946d8345cee6e3"
+  on_macos do
+    if Hardware::CPU.arm?
+      url "https://github.com/kowainik/summoner/releases/download/v2.2.0.0/summon-cli-macos-arm64"
+      sha256 "ac687412d4a51d403df514ed581d72982c7bc924710e73db6a946d8345cee6e3"
+    else
+      url "https://github.com/kowainik/summoner/releases/download/v2.2.0.0/summoner-2.2.0.0-macos-x86_64"
+      sha256 "84df0d63c5d6094256c288ca2110595a2323ec19dd5496a748946c9c8f9da486"
+    end
+  end
 
   depends_on "gh"
   conflicts_with "summoner-tui", :because => "because both install 'summon' binaries"
 
   def install
-    odie "This formula currently supports only Apple Silicon (arm64) macOS" unless Hardware::CPU.arm?
-    bin.install "summon-cli-macos-arm64" => "summon"
+    if Hardware::CPU.arm?
+      bin.install "summon-cli-macos-arm64" => "summon"
+    else
+      bin.install "summoner-2.2.0.0-macos-x86_64" => "summon"
+    end
   end
 
   test do
--- summoner-tui.rb
+++ summoner-tui.rb
@@ -1,15 +1,25 @@
 class SummonerTui < Formula
   desc "TUI Tool for scaffolding batteries-included Haskell projects"
   homepage "https://kowainik.github.io/projects/summoner"
-  url "https://github.com/kowainik/summoner/releases/download/v2.2.0.0/summon-tui-macos-arm64"
-  sha256 "b6a5a9bb8616d953969fd2171ccabc47b0d67958164cf26c95cbd54c8b3d03e2"
+  on_macos do
+    if Hardware::CPU.arm?
+      url "https://github.com/kowainik/summoner/releases/download/v2.2.0.0/summon-tui-macos-arm64"
+      sha256 "b6a5a9bb8616d953969fd2171ccabc47b0d67958164cf26c95cbd54c8b3d03e2"
+    else
+      url "https://github.com/kowainik/summoner/releases/download/v2.2.0.0/summoner-tui-2.2.0.0-macos-x86_64"
+      sha256 "75991388620092fd66a44a5c67607bc712b20cc951da5618e004207400ed7cdf"
+    end
+  end
 
   depends_on "gh"
   conflicts_with "summoner-cli", :because => "because both install 'summon' binaries"
 
   def install
-    odie "This formula currently supports only Apple Silicon (arm64) macOS" unless Hardware::CPU.arm?
-    bin.install "summon-tui-macos-arm64" => "summon"
+    if Hardware::CPU.arm?
+      bin.install "summon-tui-macos-arm64" => "summon"
+    else
+      bin.install "summoner-tui-2.2.0.0-macos-x86_64" => "summon"
+    end
   end
 
   test do
